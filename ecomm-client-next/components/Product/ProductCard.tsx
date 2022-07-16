6ff1e6ec46fc7695c297a9880bc2585fcd97ac59
Rohit Sah <sahrohit9586@gmail.com>
2022-07-16 23:07:37 +0545

    Update Product Card

--- ecomm-client-next/components/Product/ProductCard.tsx
+++ ecomm-client-next/components/Product/ProductCard.tsx
@@ -2,48 +2,41 @@
 import { ProductResponse } from "@generated/graphql";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 
 const ProductCard = ({
 	name,
-	desc,
 	images,
 	variants,
 	identifier,
+	category_name,
 }: ProductResponse) => {
-	const [imageURL, setImageURL] = useState(images[0].imageURL);
+	// const [imageURL, setImageURL] = useState(images[0].imageURL);
 
 	return (
 		<Link href={`/products/${identifier}`}>
 			<div
 				className="m-6 rounded-md flex flex-col shadow-s"
-				onMouseEnter={() =>
-					setImageURL(
-						images.length <= 1
-							? images[0].imageURL
-							: images.filter((image) => image.imageURL != imageURL)[
-									Math.floor(Math.random() * (images.length - 1))
-							  ].imageURL
-					)
-				}
+				// onMouseEnter={() =>
+				// 	setImageURL(
+				// 		images.length <= 1
+				// 			? images[0].imageURL
+				// 			: images.filter((image) => image.imageURL != imageURL)[
+				// 					Math.floor(Math.random() * (images.length - 1))
+				// 			  ].imageURL
+				// 	)
+				// }
 			>
 				<Image
-					className="rounded-t-md"
-					src={imageURL}
+					className="rounded-md"
+					src={images[0].imageURL}
 					alt={name}
-					height="600"
-					width="600"
+					height="500"
+					width="500"
 				/>
-				<div className="p-4 flex flex-col justify-between space-y-4 flex-grow border-2 border-gray-300 border-t-0 rounded-md rounded-t-none">
-					<div>
-						<h2 className="font-bold">{name}</h2>
-						<p className="text-sm">{desc}</p>
-					</div>
-
-					<div>
-						<em>{variants.length} Variants</em>
-						<p className="text-md font-bold">Rs {variants[0].price}</p>
-					</div>
+				<div className="p-4 flex flex-col justify-between space-y-1 flex-grow rounded-md rounded-t-none text-center">
+					<p className="text-sm text-slate-500">{category_name}</p>
+					<h2 className="font-bold text-xl">{name}</h2>
+					<p className="">Rs {variants[0].price}</p>
 				</div>
 			</div>
 		</Link>
