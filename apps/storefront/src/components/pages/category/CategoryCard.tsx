1b3627a66837d45ea4e2520a6c3ce6386dbb75c8
Rohit Sah <sahrohit9586@gmail.com>
2023-04-12 02:49:20 +0200

    added category page

---
+++ apps/storefront/src/components/pages/category/CategoryCard.tsx
@@ -0,0 +1,58 @@
+import {
+	AspectRatio,
+	Box,
+	Image,
+	Skeleton,
+	Stack,
+	StackProps,
+	Text,
+} from "@chakra-ui/react";
+import { Link } from "@chakra-ui/next-js";
+
+interface CategoryCardProps {
+	category: {
+		id: string;
+		name: string;
+		imageUrl: string;
+	};
+	rootProps?: StackProps;
+}
+
+export const CategoryCard = (props: CategoryCardProps) => {
+	const { category, rootProps } = props;
+	const { id, name, imageUrl } = category;
+	return (
+		<Stack spacing={{ base: "4", md: "5" }} {...rootProps}>
+			<Box position="relative">
+				<AspectRatio ratio={1 / 1}>
+					<Image
+						src={imageUrl}
+						alt={name}
+						draggable="false"
+						fallback={<Skeleton borderRadius={{ base: "md", md: "xl" }} />}
+						borderRadius={{ base: "md", md: "xl" }}
+					/>
+				</AspectRatio>
+				<Link
+					href={`/category/${id}`}
+					position={"absolute"}
+					inset={0}
+					bgGradient="linear(to-b, transparent 60%, gray.900)"
+					borderRadius={{ base: "md", md: "xl" }}
+				/>
+				<Text
+					position="absolute"
+					w={"full"}
+					textAlign={"center"}
+					bottom={6}
+					whiteSpace={"nowrap"}
+					fontSize={"xl"}
+					fontWeight={"semibold"}
+					color={"white"}
+				>
+					{name}
+				</Text>
+			</Box>
+		</Stack>
+	);
+};
---
+++ apps/storefront/src/pages/category/index.tsx
@@ -0,0 +1,29 @@
+import { CategoryCard } from "@/components/pages/category/CategoryCard";
+import { CategoryGrid } from "@/components/pages/category/CategoryGrid";
+import Footer from "@/components/shared/Footer";
+import { Navbar } from "@/components/shared/navbar";
+import { CATEGORIES } from "@/data/mock/category";
+import { Box, HStack, Heading } from "@chakra-ui/react";
+
+const CategoryPage = () => {
+	return (
+		<>
+			<Navbar />
+			<Box mx={"32"}>
+				<HStack my={8}>
+					<Heading fontSize={"3xl"} lineHeight={"1.2"}>
+						Shop by Categories
+					</Heading>
+				</HStack>
+				<CategoryGrid>
+					{CATEGORIES.map((category) => (
+						<CategoryCard key={category.id} category={category} />
+					))}
+				</CategoryGrid>
+			</Box>
+			<Footer />
+		</>
+	);
+};
+
+export default CategoryPage;
