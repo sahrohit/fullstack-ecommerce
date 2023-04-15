a32b7d0a77bdb3eb1beeded0c2423e7fb80bb431
Rohit Sah <sahrohit9586@gmail.com>
2023-04-15 21:35:31 +0200

    refactor(components): :lipstick: refactored address component

--- apps/storefront/src/pages/account.tsx
+++ apps/storefront/src/pages/account.tsx
@@ -1,6 +1,6 @@
 import AccountPreference from "@/components/pages/account/AccountPreference";
 import AccountSettings from "@/components/pages/account/AccountSetting";
-import Address from "@/components/pages/account/Address";
+import AddressSection from "@/components/pages/account/Address";
 import DangerZone from "@/components/pages/account/DangerZone";
 import HeadingGroup from "@/components/pages/account/HeadingGroup";
 import SocialAccountSettings from "@/components/pages/account/SocialAccountSettings";
@@ -10,14 +10,14 @@ import { Box, Stack } from "@chakra-ui/react";
 const AccountPage = () => (
 	<>
 		<Navbar />
-		<Box maxW="3xl" mx="auto" my={8}>
+		<Box maxW="3xl" my={8} mx={{ base: 4, md: "auto" }}>
 			<Stack as="section" spacing="6">
 				<HeadingGroup
 					title="Account Settings"
 					description="Change your profile, request your data, and more"
 				/>
 				<AccountSettings />
-				<Address />
+				<AddressSection />
 				<AccountPreference />
 				<SocialAccountSettings />
 				<DangerZone />
