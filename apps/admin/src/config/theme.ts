8efdd267e9975754109c796c625e04e7c4f5d61c
Rohit Sah <sahrohit9586@gmail.com>
2023-04-20 00:35:27 +0200

    build(pages): :construction: admin init

---
+++ apps/admin/src/config/theme.ts
@@ -0,0 +1,39 @@
+import { extendTheme, ColorMode } from "@chakra-ui/react";
+import { theme as proTheme } from "@chakra-ui/pro-theme";
+
+interface IThemeMode {
+	Light: ColorMode;
+	Dark: ColorMode;
+}
+
+export const ThemeMode: IThemeMode = {
+	Light: "light",
+	Dark: "dark",
+};
+
+export const mobileBreakpointsMap = {
+	base: true,
+	md: true,
+	lg: true,
+	xl: false,
+};
+
+// Theme Config
+const config = {
+	initialColorMode: ThemeMode.Dark,
+	useSystemColorMode: true,
+};
+
+const theme = extendTheme({
+	config,
+	...proTheme,
+	styles: {
+		global: {
+			body: {
+				transitionProperty: "all",
+				transitionDuration: "normal",
+			},
+		},
+	},
+});
+export default theme;
--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -1,8 +1,22 @@
+import theme from "@/config/theme";
 import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-const App = ({ Component, pageProps }: AppProps) => (
-	<Component {...pageProps} />
-);
+const App = ({ Component, pageProps }: AppProps) => {
+	const client = new ApolloClient({
+		uri: "http://localhost:4000/graphql",
+		cache: new InMemoryCache(),
+	});
+
+	return (
+		<ChakraProvider theme={theme}>
+			<ApolloProvider client={client}>
+				<Component {...pageProps} />
+			</ApolloProvider>
+		</ChakraProvider>
+	);
+};
 
 export default App;
