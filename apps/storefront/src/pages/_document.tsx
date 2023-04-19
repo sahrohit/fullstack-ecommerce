03dc27531c45804f478c5b96bdc92a01597e262d
Rohit Sah <sahrohit9586@gmail.com>
2023-04-20 00:36:26 +0200

    fix(pages): :adhesive_bandage: fixed colormode scrpit issue

--- apps/storefront/src/pages/_document.tsx
+++ apps/storefront/src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
-import theme from "@/config/theme";
+import theme from "../config/theme";
 
 const Document = () => (
 	<Html lang="en">
