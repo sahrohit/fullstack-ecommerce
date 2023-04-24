192631f5890e332378e4acc8f4eba73f3218e002
Rohit Sah <sahrohit9586@gmail.com>
2023-04-24 02:09:17 +0200

    feat(pages): :white_check_mark: added verify email pae

---
+++ apps/storefront/src/pages/auth/verify-email/[token].tsx
@@ -0,0 +1,65 @@
+import PageLoader from "@/components/shared/PageLoader";
+import Result from "@/components/shared/Result";
+import { useVerifyEmailMutation } from "@/generated/graphql";
+import { Link } from "@chakra-ui/next-js";
+import { Button } from "@chakra-ui/react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const VerifyEmailPage: NextPage = () => {
+	const router = useRouter();
+	const { token } = router.query;
+	const [emailVerified, setEmailVerified] = useState<
+		"loading" | "verified" | "not-verified"
+	>("loading");
+
+	const [verifyEmail] = useVerifyEmailMutation();
+
+	useEffect(() => {
+		const asyncFuntion = async () => {
+			const response = await verifyEmail({
+				variables: {
+					token: token as string,
+				},
+			});
+			if (response.data?.verifyEmail) {
+				setEmailVerified("verified");
+				router.replace("/");
+				return;
+			}
+			setEmailVerified("not-verified");
+		};
+		if (typeof window !== "undefined" && token) {
+			asyncFuntion();
+		}
+	}, [router, token, verifyEmail]);
+
+	if (emailVerified === "loading") {
+		return <PageLoader />;
+	}
+
+	if (emailVerified === "not-verified") {
+		return (
+			<Result
+				type="error"
+				heading="Invalid Token"
+				text="Please regenerate verification link here."
+			/>
+		);
+	}
+
+	return (
+		<Result
+			type="success"
+			heading="Successfully Verified"
+			text="Your account is successfully verifed. You can now login."
+		>
+			<Button as={Link} href="/" textDecoration="none!">
+				Go to Dashboard
+			</Button>
+		</Result>
+	);
+};
+
+export default VerifyEmailPage;
