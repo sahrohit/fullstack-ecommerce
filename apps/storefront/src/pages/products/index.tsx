48b842e46215db3356e6d915a7f2570d2dab67d9
Rohit Sah <sahrohit9586@gmail.com>
2023-05-03 02:24:48 +0200

    fix(pages): :bug: fixed the products page

--- apps/storefront/src/pages/products/index.tsx
+++ apps/storefront/src/pages/products/index.tsx
@@ -2,21 +2,39 @@ import ProductCard from "@/components/pages/product/ProductCard";
 import ProductGrid from "@/components/pages/product/ProductGrid";
 import FilterLayout from "@/components/pages/product/filter/FilterLayout";
 import Footer from "@/components/shared/Footer";
+import Result from "@/components/shared/Result";
 import Navbar from "@/components/shared/navbar";
-import { products } from "@/data/temp";
+import { useProductsQuery } from "@/generated/graphql";
 
-const ProductFilterPage = () => (
-	<>
-		<Navbar />
-		<FilterLayout>
-			<ProductGrid>
-				{[...products, ...products, ...products].map((product) => (
-					<ProductCard key={product.id} product={product} />
-				))}
-			</ProductGrid>
-		</FilterLayout>
-		<Footer />
-	</>
-);
+const ProductFilterPage = () => {
+	const { data, loading, error } = useProductsQuery();
+
+	if (loading) return <div>Loading...</div>;
+
+	if (error)
+		return (
+			<Result
+				heading={error.name}
+				type="error"
+				text={error.message}
+				dump={error.stack}
+			/>
+		);
+
+	return (
+		<>
+			<Navbar />
+			<FilterLayout>
+				<ProductGrid>
+					{data?.products &&
+						data.products.map((product) => (
+							<ProductCard key={product.id} product={product} />
+						))}
+				</ProductGrid>
+			</FilterLayout>
+			<Footer />
+		</>
+	);
+};
 
 export default ProductFilterPage;
