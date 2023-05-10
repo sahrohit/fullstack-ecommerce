07c51b549076d27da7938c6f2ada20cf24f65896
Rohit Sah <sahrohit9586@gmail.com>
2023-05-11 00:12:46 +0200

    feat(storefront): :sparkles: khalti payment integration init

--- apps/storefront/constants.ts
+++ apps/storefront/constants.ts
@@ -1,2 +1,8 @@
+export const PROD = process.env.NODE_ENV === "production";
+
 export const BRAND_NAME = "Ecommerce Storefront";
 export const COLOR = "Ecommerce Storefront";
+
+export const APP_URL = !PROD
+	? "http://localhost:3000"
+	: "https://hamropasal.vercel.app";
---
+++ apps/storefront/src/pages/api/payment.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { APP_URL } from "../../../constants";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<any>
+) {
+	if (req.method !== "POST") {
+		res.status(405).send({ message: "Only POST requests allowed" });
+		return;
+	}
+
+	const resposne = await fetch(
+		"https://a.khalti.com/api/v2/epayment/initiate/",
+		{
+			method: "POST",
+			body: JSON.stringify({
+				return_url: `${APP_URL}/payment/success`,
+				website_url: APP_URL,
+				...req.body,
+			}),
+			headers: {
+				"content-type": "application/json",
+				Authorization: `Key ${process.env.KHALTI_SECRET_KEY}`, // Replace LIVE_SECRET_KEY with your live secret key
+			},
+		}
+	);
+
+	const khaltiResponse = await resposne.json();
+
+	res.status(200).json({ ...khaltiResponse });
+}
