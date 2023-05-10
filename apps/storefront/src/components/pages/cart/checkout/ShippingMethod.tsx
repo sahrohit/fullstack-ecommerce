36e41ff7890a54ca0b84bdd0b48e2691ff4e0456
Rohit Sah <sahrohit9586@gmail.com>
2023-05-10 02:20:47 +0200

    feat(storefront): :sparkles: added shipping method selector

---
+++ apps/storefront/src/components/pages/cart/checkout/ShippingMethod.tsx
@@ -0,0 +1,47 @@
+import {
+	RadioGroup,
+	Stack,
+	Radio,
+	UseRadioGroupProps,
+	VStack,
+	Heading,
+	Box,
+	Text,
+} from "@chakra-ui/react";
+
+interface ShippingMethodProps extends UseRadioGroupProps {
+	options: {
+		title: string;
+		desc: string;
+		value: string;
+	}[];
+}
+
+const ShippingMethod = ({ value, onChange, options }: ShippingMethodProps) => (
+	<Box as="section" py="4" w="full">
+		<Heading fontSize="xl" fontWeight="bold" lineHeight="1.2" my={4}>
+			Shipping Method
+		</Heading>
+		<RadioGroup onChange={onChange} value={value}>
+			<Stack
+				direction={["column", "row"]}
+				w="full"
+				justifyContent="space-evenly"
+				flexWrap="wrap"
+			>
+				{options.map((option) => (
+					<Radio value={option.value}>
+						<VStack alignItems="left" ml={2}>
+							<Text fontSize="lg" fontWeight="bold" lineHeight="1">
+								{option.title}
+							</Text>
+							<Text fontSize="lg">{option.desc}</Text>
+						</VStack>
+					</Radio>
+				))}
+			</Stack>
+		</RadioGroup>
+	</Box>
+);
+
+export default ShippingMethod;
