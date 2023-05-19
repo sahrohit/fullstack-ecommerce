350692d08493123b5bd111f1baaa4bc520ecf2a7
Rohit Sah <sahrohit9586@gmail.com>
2023-05-19 17:03:19 +0200

    refactor(storefront): :recycle: removed the filter list label when no filters are applied

--- apps/storefront/src/components/pages/product/filter/FilterOptions.tsx
+++ apps/storefront/src/components/pages/product/filter/FilterOptions.tsx
@@ -32,56 +32,58 @@ const FilterOptions = ({
 }: FilterOptionsProps) => (
 	<VStack px={{ base: 4, sm: 6, md: 8 }} {...props}>
 		<VStack gap={4} position="sticky" top={4}>
-			<SimpleGrid spacing={2} w="full">
-				<HStack w="full" justifyContent="space-between">
-					<FormLabel>Selected Options</FormLabel>
-					{selectedVariant && Object.keys(selectedVariant).length && (
-						<Tooltip placement="top" label="Clear all selected options">
-							<CloseButton
-								onClick={() => {
-									setSelectedVariant({});
-								}}
-							/>
-						</Tooltip>
-					)}
-				</HStack>
-				{selectedVariant &&
-					Object.keys(selectedVariant).map((item: string) => (
-						<Tag
-							size="lg"
-							w="full"
-							key={item}
-							variant="subtle"
-							py={1}
-							justifyContent="space-between"
-						>
-							<TagLabel>
-								{capitalize(item)} :{" "}
-								{typeof selectedVariant[`${item}`] === "string" ? (
-									capitalize(selectedVariant[`${item}`] as string)
-								) : (
-									<PriceTag
-										price={Number(selectedVariant[`${item}`])}
-										currency="NPR"
-										rootProps={{
-											display: "inline-flex",
-										}}
-									/>
-								)}
-							</TagLabel>
-							<TagCloseButton
-								onClick={() => {
-									setSelectedVariant(
-										(prev: Record<string, string | number>) => {
-											const { [item]: omit, ...rest } = prev;
-											return rest;
-										}
-									);
-								}}
-							/>
-						</Tag>
-					))}
-			</SimpleGrid>
+			{Object.keys(selectedVariant).length && (
+				<SimpleGrid spacing={2} w="full">
+					<HStack w="full" justifyContent="space-between">
+						<FormLabel>Selected Options</FormLabel>
+						{Object.keys(selectedVariant).length && (
+							<Tooltip placement="top" label="Clear all selected options">
+								<CloseButton
+									onClick={() => {
+										setSelectedVariant({});
+									}}
+								/>
+							</Tooltip>
+						)}
+					</HStack>
+					{selectedVariant &&
+						Object.keys(selectedVariant).map((item: string) => (
+							<Tag
+								size="lg"
+								w="full"
+								key={item}
+								variant="subtle"
+								py={1}
+								justifyContent="space-between"
+							>
+								<TagLabel>
+									{capitalize(item)} :{" "}
+									{typeof selectedVariant[`${item}`] === "string" ? (
+										capitalize(selectedVariant[`${item}`] as string)
+									) : (
+										<PriceTag
+											price={Number(selectedVariant[`${item}`])}
+											currency="NPR"
+											rootProps={{
+												display: "inline-flex",
+											}}
+										/>
+									)}
+								</TagLabel>
+								<TagCloseButton
+									onClick={() => {
+										setSelectedVariant(
+											(prev: Record<string, string | number>) => {
+												const { [item]: omit, ...rest } = prev;
+												return rest;
+											}
+										);
+									}}
+								/>
+							</Tag>
+						))}
+				</SimpleGrid>
+			)}
 			{variants.map((item) => {
 				if (item.variant_values.length === 0) return null;
 				if (item.variant_name === "color") {
