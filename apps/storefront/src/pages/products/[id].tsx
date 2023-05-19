a14ad909742e67b181cd279675759250a80368c0
Rohit Sah <sahrohit9586@gmail.com>
2023-05-19 23:15:55 +0200

    style(storefront): :lipstick: empty state for product not found

--- apps/storefront/src/pages/products/[id].tsx
+++ apps/storefront/src/pages/products/[id].tsx
@@ -3,8 +3,17 @@ import ProductReview from "@/components/pages/product/review/ProductReview";
 import PageLoader from "@/components/shared/PageLoader";
 import Result from "@/components/shared/Result";
 import Navbar from "@/components/shared/navbar";
+import UnderlineLink from "@/components/ui/UnderlineLink";
 import { Product, useProductByIdQuery } from "@/generated/graphql";
-import { VStack } from "@chakra-ui/react";
+import {
+	Box,
+	HStack,
+	Heading,
+	Img,
+	SimpleGrid,
+	Text,
+	VStack,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 const ProductPage = () => {
@@ -36,10 +45,34 @@ const ProductPage = () => {
 	return (
 		<>
 			<Navbar />
-			<VStack p={{ base: 4, md: 8, lg: 8, xl: 16 }} spacing={8}>
-				{data && <ProductDetails product={data.product as Product} />}
-				<ProductReview />
-			</VStack>
+			{data?.product ? (
+				<VStack p={{ base: 4, md: 8, lg: 8, xl: 16 }} spacing={8}>
+					{data && <ProductDetails product={data.product as Product} />}
+					<ProductReview />
+				</VStack>
+			) : (
+				<SimpleGrid placeItems="center" w="full" h="80vh">
+					<VStack w="full" py={8} gap={12} maxW="3xl">
+						<Box textAlign="center">
+							<Heading as="h3" fontSize="2xl" lineHeight="1">
+								Product Not Found
+							</Heading>
+							<Text>
+								This product maybe removed or temporarily not available.
+							</Text>
+						</Box>
+						<HStack>
+							<UnderlineLink href="/">Go Back Home</UnderlineLink>
+						</HStack>
+						<Img
+							width="50%"
+							placeholder="blur"
+							alt="App screenshot"
+							src="/assets/delete-confirmation.svg"
+						/>
+					</VStack>
+				</SimpleGrid>
+			)}
 		</>
 	);
 };
