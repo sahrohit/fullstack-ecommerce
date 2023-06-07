b7b78d77a811c9a455f80866de88e1e797959775
Rohit Sah <sahrohit9586@gmail.com>
2023-06-07 19:44:44 +0200

    fix(storefront): :bug: added options to fix deployment

---
+++ apps/storefront/src/data/options.ts
@@ -0,0 +1,15 @@
+const BRANDS = [
+	{ id: "adidas", name: "Adidas" },
+	{ id: "nike", name: "Nike" },
+	{ id: "puma", name: "Puma" },
+	{ id: "reebok", name: "Reebok" },
+	{ id: "vans", name: "Vans" },
+	{ id: "converse", name: "Converse" },
+	{ id: "newbalance", name: "New Balance" },
+	{ id: "asics", name: "Asics" },
+	{ id: "saucony", name: "Saucony" },
+	{ id: "armour", name: "Under Armour" },
+	{ id: "brooks", name: "Brooks" },
+];
+
+export default BRANDS;
