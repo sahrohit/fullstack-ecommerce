693cfa27dba25013c8ce44995bc191adc5423bae
avintimilsina <avin.timilsina.16@gmail.com>
2023-09-23 22:05:25 +0545

    chore(storefront): :lipstick: added footer to individual product page

--- apps/storefront/src/components/pages/product/ProductDetails.tsx
+++ apps/storefront/src/components/pages/product/ProductDetails.tsx
@@ -1,6 +1,7 @@
 import { Stack, VStack, HStack, Heading, Box } from "@chakra-ui/react";
 import { Product, ProductImage } from "@/generated/graphql";
 import Rating from "@/components/shared/product/Rating";
+import Footer from "@/components/shared/Footer";
 import Carousel from "./Carousel";
 import ImageGrid from "./ImageGrid";
 import AddToCartForm from "./AddToCartForm";
@@ -13,29 +14,32 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
 	const images = [...product.images]?.sort((a, b) => a.sequence - b.sequence);
 
 	return (
-		<Stack w="full" direction={{ base: "column-reverse", lg: "row" }} gap={8}>
-			<VStack gap={2} alignItems="flex-start">
-				<HStack>
-					<Rating defaultValue={2} /> <span>12 reviews</span>
-				</HStack>
-				<Heading fontSize="3xl" lineHeight={1.2}>
-					{product.name}
-				</Heading>
+		<>
+			<Stack w="full" direction={{ base: "column-reverse", lg: "row" }} gap={8}>
+				<VStack gap={2} alignItems="flex-start">
+					<HStack>
+						<Rating defaultValue={2} /> <span>12 reviews</span>
+					</HStack>
+					<Heading fontSize="3xl" lineHeight={1.2}>
+						{product.name}
+					</Heading>
 
-				{product.inventories && <AddToCartForm product={product} />}
-			</VStack>
-			<Box
-				maxW={
-					images && images.length <= 3 ? { base: "100%", lg: "50%" } : "100%"
-				}
-			>
-				{images[0] && images.length > 3 ? (
-					<Carousel images={images.map(imageMapper)} />
-				) : (
-					<ImageGrid images={images.map(imageMapper)} />
-				)}
-			</Box>
-		</Stack>
+					{product.inventories && <AddToCartForm product={product} />}
+				</VStack>
+				<Box
+					maxW={
+						images && images.length <= 3 ? { base: "100%", lg: "50%" } : "100%"
+					}
+				>
+					{images[0] && images.length > 3 ? (
+						<Carousel images={images.map(imageMapper)} />
+					) : (
+						<ImageGrid images={images.map(imageMapper)} />
+					)}
+				</Box>
+			</Stack>
+			<Footer />
+		</>
 	);
 };
 
