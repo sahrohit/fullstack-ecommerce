e961889603a520949102704d14f4d51ba9fe743d
Rohit Sah <sahrohit9586@gmail.com>
2023-10-28 15:54:35 +0545

    fix(api): :ambulance: fixed init migration

--- apps/api/src/migration/1694454773982-Init.ts
+++ apps/api/src/migration/1694454773982-Init.ts
@@ -9,8 +9,14 @@ export class Init1694454773982 implements MigrationInterface {
         INSERT INTO public.user_role (id, name) VALUES (4, 'MANAGER');
         INSERT INTO public.user_role (id, name) VALUES (5, 'ADMIN');
 
-        INSERT INTO public.shipping_method (name, price, dispatch_in) VALUES ( 'Standard Delivery', 8000, 96 );
-        INSERT INTO public.shipping_method (name, price, dispatch_in) VALUES ( 'Express Delivery', 20000, 24);
+        INSERT INTO public."user" (id, password, first_name, last_name, email, email_verified, phone_number, phone_number_verified, "imageUrl", "roleId", created_at, updated_at, language, currency, marketing_product_news, marketing_company_news) VALUES (11, '$argon2id$v=19$m=65536,t=3,p=4$tQqlPwiYBoIBf2HGDYi2TQ$o9/FBb8IcO3q63Rqs9N95rRkwR5Mt48YMlIPFY3fkmo', 'Admin', 'Admin', 'admin@admin.com', false, null, false, 'https://api.dicebear.com/6.x/micah/svg?size=256&seed=Admin', 1, '2023-10-28 15:50:07.533245', '2023-10-28 15:50:07.533245', 'en', 'NPR', false, false);
+
+        INSERT INTO public.tenant_category (id, name, identifier, "desc", created_at, updated_at) VALUES (1, 'General', 'general', 'General Purpose Store', '2023-09-17 15:26:28.682800', '2023-09-17 15:26:28.682800');
+
+        INSERT INTO tenant (name, "desc", address, subdomain, "customDomain", "userId", "categoryId") VALUES ('Adminpasal', 'This is a shop created by admin for testing','Mero Ghar, Earth','adminpasal',null,1,1)
+
+        INSERT INTO public.shipping_method (name, price, dispatch_in, "tenantId") VALUES ( 'Standard Delivery', 8000, 96,1 );
+        INSERT INTO public.shipping_method (name, price, dispatch_in,"tenantId") VALUES ( 'Express Delivery', 20000, 24,1);
 
         INSERT INTO public.issue_category (name, identifier, "desc")
         VALUES
